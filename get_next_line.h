3e997db8c1b1388c6c83aecc967202fd119c74ef
Mthokozisi NZOLO <mnzolo@c5r2s11.wethinkcode.co.za>
2019-07-30 16:07:57 +0200

    get_next_line.h

--- get_next_line.h
+++ get_next_line.h
@@ -6,20 +6,16 @@
 /*   By: mnzolo <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/25 10:04:11 by mnzolo            #+#    #+#             */
-/*   Updated: 2019/07/04 10:04:28 by mnzolo           ###   ########.fr       */
+/*   Updated: 2019/07/08 13:17:05 by mnzolo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-# define BUFF_SIZE 32
+# define BUFF_SIZE 10
 
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/uio.h>
-#include <fcntl.h>
-#include "./libft/libft.h"
+# include <unistd.h>
+# include "./libft/libft.h"
 
-int     get_next_line(const int fd, char **line);
-
-# endif
+int	get_next_line(const int fd, char **line);
+#endif
